4b6f48b3c9ed764b7067b7b6a700ef33a218e1c3
wesley sprague <wesleysprague@yahoo.com>
2019-01-16 11:08:10 -0500

    created view tests and createbucketview

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
 
+class CreateView(generics.listCreateAPIView):
+  """This class defines the creation behavior of our rest API"""
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+
+    def perform_create(self, serializer):
+      """Saves post data when new bucketlist is created"""
+      serializer.save()
 # Create your views here.
