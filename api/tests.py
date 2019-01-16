7c748940069fe6d0976fac35abfe20d7d4bea8f8
wesley sprague <wesleysprague@yahoo.com>
2019-01-16 12:07:40 -0500

    specified url endpoints

--- api/tests.py
+++ api/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from .models import Bucketlist
 from rest_framework.test import APIClient
 from rest_framework import status
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 # Create your tests here.
 class ModelTestCase(TestCase):
@@ -20,16 +20,19 @@ class ModelTestCase(TestCase):
         self.assertNotEqual(old_count, new_count)
 
 class ViewTestCase(TestCase):
-  """Test suite for API Views"""
+    """Test suite for the api views."""
+
     def setUp(self):
-        """Define the test client and other test variables"""
+        """Define the test client and other test variables."""
         self.client = APIClient()
         self.bucketlist_data = {'name': 'Go to Ibiza'}
         self.response = self.client.post(
-          reverse('create'),
-          self.bucketlist_data,
-          format="json")
+            reverse('create'),
+            self.bucketlist_data,
+            format="json")
 
     def test_api_can_create_a_bucketlist(self):
-        """Test the api has bucket creation ability"""
+        """Test the api has bucket creation capability."""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+
